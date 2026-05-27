b77e52f1cf1ff172e0928d86508243bdb1e2c773
KarOsk2007 <abvgditsukengshshzh@yandex.ru>
2026-05-28 01:15:56 +0700

    chore: initial project setup

---
+++ main.c
@@ -0,0 +1,15 @@
+﻿#include "raylib.h"
+
+int main(void)
+{
+    InitWindow(800, 600, "Sweeper");
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(DARKGRAY);
+        DrawText("Hello, Sweeper!", 300, 200, 30, WHITE);
+        EndDrawing();
+    }
+    CloseWindow();
+    return 0;
+}
